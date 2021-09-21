5091dc6b64f26ff0c978c4b169449a107505f28a
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-09-21 16:57:30 +0200

    Added home page and styled it.

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ export default function App() {
     <div className="App">
       <Header/>
       <Home/>
-      <PackagesList />
-      <PackageItem/>
+      {/* <PackagesList />
+      <PackageItem/> */}
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
 export default function Home() {
     return (
-        <div>
+        <div className='home-wrapper'>
             <h1>Track your packages</h1>
             <p>Are you expecting delivery of a package? Great! Click the button below to track your packages.</p>
+            <button>Track your packages</button>
         </div>
     )
 }
