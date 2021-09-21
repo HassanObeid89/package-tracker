f9a08f02e5cd9ba7d9b1d19902f58d35293563e0
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-09-21 15:28:28 +0200

    Project structure

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card() {
+    return (
+        <div>
+            <li>
+                <p>icon</p>
+                <span></span>
+                <label></label>
+            </li>
+        </div>
+    )
+}
