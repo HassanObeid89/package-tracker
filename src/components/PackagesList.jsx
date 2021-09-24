077361ce0963f214b8f74c8d32dab53cff1295f2
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-09-24 14:24:31 +0200

    Catched the error

--- src/components/PackagesList.jsx
+++ src/components/PackagesList.jsx
@@ -1,12 +1,17 @@
 import Card from "./Card";
 import HashLoader from "react-spinners/HashLoader";
 
-export default function PackagesList({loading, data,error}) {
-  if(loading) return <div className='spinner'><HashLoader /></div>;
-  const cards = data.map((parcel) => <Card key={parcel.id} parcel={parcel} />)
+export default function PackagesList({ loading, data, error }) {
+  if (loading)
+    return (
+      <div className="spinner">
+        <HashLoader />
+      </div>
+    );
+  if (error) return <p>ooopppss</p>;
+  const cards = data.map((parcel) => <Card key={parcel.id} parcel={parcel} />);
   return (
     <div className="packages-wrapper">
-      {error && JSON.stringify(error, null, 2)} 
       <ul>{cards}</ul>
     </div>
   );
--- src/scripts/useFetch.js
+++ src/scripts/useFetch.js
@@ -1,24 +1,3 @@
-
-// export function useFetch(uri) {
-
-//   const [data, setData] = useState(getDataLS());
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState();
-
-// useEffect(() => {
-//   if (!uri) return;
-//   setTimeout(() => {
-//     fetch(uri)
-//       .then((response) => response.json())
-//       .then(setData)
-//       .then(() => setLoading(false))
-//       .then(() => localStorage.setItem("cards", JSON.stringify(data)))
-//       .catch(setError);
-//   }, 1000);
-// }, [uri]);
-
-//   return { loading, data, error };
-// }
 import { useState, useEffect } from "react";
 export function useFetch(uri) {
   const [data, setData] = useState({});
@@ -26,39 +5,41 @@ export function useFetch(uri) {
   const [error, setError] = useState();
 
   useEffect(() => {
-    //const uri = "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840";
-  //check the cache
-  if (!uri) return;
-  setTimeout(() => {
-  if ("caches" in window) {
-    window.caches.open("my-data-caches").then((cache) => {
-      cache.match(uri).then((result) => {
-        if (result === undefined) {
-          //fetch the request normally
-          fetcher(uri, cache)
-        } else {
-          result.json().then((cachedData) => {
-            setData(cachedData);
-            setLoading(false)
+    //check the cache
+    if (!uri) return;
+    setTimeout(() => {
+      if ("caches" in window) {
+        window.caches.open("my-data-caches").then((cache) => {
+          cache.match(uri).then((result) => {
+            if (result === undefined) {
+              //fetch the request normally
+              fetcher(uri, cache);
+            } else {
+              result.json().then((cachedData) => {
+                setData(cachedData);
+                setLoading(false);
+              });
+            }
           });
-        }
-      });
-    });
-  }
-}, 2000);
-  }, [])
-  
-  return { loading, data };
+        });
+      }
+    }, 2000);
+  }, []);
+
+  return { loading, data, error };
 
   function fetcher(uri, cache) {
     fetch(uri).then((result) => {
       //make a copy
       let clonedResponse = result.clone();
       cache.put(uri, result);
-      clonedResponse.json().then((data) => {
-        setData(data);
-        setLoading(false)
-      });
+      clonedResponse
+        .json()
+        .then((data) => {
+          setData(data);
+          setLoading(false);
+        })
+        .catch(setError);
     });
   }
 }
